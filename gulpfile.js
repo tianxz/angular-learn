1cea611972e718901b5fd159071253ae64bc1ee8
mt <oc.mt@hotmail.com>
2017-06-28 20:57:17 +0800

    ...

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('default', ['copy']);
+
+gulp.task('start', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'src/'
+        }
+    })
+});
+
+gulp.task('copy', function () {
+    //angular
+    gulp.src(
+        [
+            createPath.bower_components('angular/angular.js')
+            , createPath.bower_components('angular/angular.min.js')
+            , createPath.bower_components('angular/angular.min.js.map')
+        ])
+        .pipe(gulp.dest(createPath.vendor('/angular')));
+
+    // jquery: {pathName: 'jquery', pathValue: 'vendor/jquery/jquery'},
+});
+
+var createPath = {
+    bower_components: function (name) {
+        return 'bower_components/' + name;
+    },
+    vendor: function (name) {
+        return 'src/vendor/' + name;
+    }
+};
